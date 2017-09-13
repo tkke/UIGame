70a26e6877f530f88747a78101d114ab8b846a59
高橋賢 <ken@ld9-Ken-Takahashi.local>
2017-09-13 14:09:19 +0900

    init commit

---
+++ Assets/ScoreScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreScript : MonoBehaviour
+{
+
+	public Text showScore;
+	public int score = 0;
+	public bool flg = false;
+
+
+	// Use this for initialization
+	void Start ()
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (flg)
+			plius ();
+	}
+
+	public void plius ()
+	{
+		score++;
+		showScore.text = score.ToString ();
+	}
+
+	public void minus ()
+	{
+		score--;
+		showScore.text = score.ToString ();
+	}
+
+	public void	Change_Flg ()
+	{
+		flg = !flg;
+	}
+}
